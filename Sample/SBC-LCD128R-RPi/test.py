4b8b82a869facb227fb09705678b10a8d35275ff
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-06-17 18:11:59 +0200

    added Args

--- Sample/SBC-LCD128R-RPi/test.py
+++ Sample/SBC-LCD128R-RPi/test.py
@@ -1,34 +1,39 @@
 import time
 import spidev as SPI
 from lib import LCD_1inch28
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image,ImageDraw,ImageFont,ImageOps
+import sys
 
+def main():
+    # Raspberry Pi pin configuration:
+    RST = 27
+    DC = 25
+    BL = 18
+    bus = 0 
+    device = 0 
+    try:
+        # display with hardware SPI:
+        disp = LCD_1inch28.LCD_1inch28()
+        # Initialize library.
+        disp.Init()
+        # Clear display.
+        disp.clear()
 
+        # Create blank image for drawing.
+        image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+        draw = ImageDraw.Draw(image1)
 
-# Raspberry Pi pin configuration:
-RST = 27
-DC = 25
-BL = 18
-bus = 0 
-device = 0 
-try:
-    # display with hardware SPI:
-    disp = LCD_1inch28.LCD_1inch28()
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
+        # Reference font file and draw text
+        Font = ImageFont.truetype("SourceSansPro.ttf",35)
+        draw.text((70, 50), ' '.join(sys.argv[1:]), fill = (255,255,255),font=Font)  # Change fill color to white
 
-    # Create blank image for drawing.
-    image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
-    draw = ImageDraw.Draw(image1)
-    
-    # Reference font file and draw text
-    Font = ImageFont.truetype("SourceSansPro.ttf",35)
-    draw.text((70, 50), 'AR-Brille', fill = (255,255,255),font=Font)  # Change fill color to white
-    
-    disp.ShowImage(image1)
-    time.sleep(50000)
-except KeyboardInterrupt:
-    disp.module_exit()
-    exit()
+        image1 = ImageOps.mirror(image1)
+
+        disp.ShowImage(image1)
+        time.sleep(50000)
+    except KeyboardInterrupt:
+        disp.module_exit()
+        exit()
+
+if __name__ == "__main__":
+    main()
