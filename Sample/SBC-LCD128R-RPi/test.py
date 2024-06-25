38be8e96e91f050c773c254ed12492110cff938d
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-06-25 12:00:22 +0200

    bt test

--- Sample/SBC-LCD128R-RPi/test.py
+++ Sample/SBC-LCD128R-RPi/test.py
@@ -3,8 +3,37 @@ import spidev as SPI
 from lib import LCD_1inch28
 from PIL import Image,ImageDraw,ImageFont,ImageOps
 import sys
+import pybluz
 
-def main():
+
+def open_bluetooth_server_socket():
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    port = 1
+    server_sock.bind(("", port))
+    server_sock.listen(1)
+
+    print("Bluetooth Server Socket Opened")
+    print("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if not data:
+                break
+            print("Received: %s" % data)
+    except IOError:
+        pass
+    except KeyboardInterrupt:
+        print("Server is closing.")
+    finally:
+        client_sock.close()
+        server_sock.close()
+
+
+def display_text(text):
     # Raspberry Pi pin configuration:
     RST = 27
     DC = 25
@@ -25,7 +54,7 @@ def main():
 
         # Reference font file and draw text
         Font = ImageFont.truetype("SourceSansPro.ttf",35)
-        draw.text((70, 50), ' '.join(sys.argv[1:]), fill = (255,255,255),font=Font)  # Change fill color to white
+        draw.text((70, 50), ' '.join(text), fill = (255,255,255),font=Font)  # Change fill color to white
 
         image1 = ImageOps.mirror(image1)
 
@@ -34,6 +63,16 @@ def main():
     except KeyboardInterrupt:
         disp.module_exit()
         exit()
+
+def main():
+    try:
+        open_bluetooth_server_socket()
+        display_text("Connected")
+    except:
+        display_text("Error")
+
+
+    #display_text(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
